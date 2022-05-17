d40f49134dbcca79bd1ea988f8e8c1a5912f7a48
4Shire <t-grishe@yandex.ru>
2022-05-18 01:47:56 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int initialAmount = 100;               // Сумма на счету абонента
+        int depositAmount = 2800;              // Сумма пополнения
+        int niceBonus;
+
+        if (depositAmount > 1000) {            // Бонус есть
+
+            System.out.println("Ваш бонус за пополнение - " + (niceBonus = depositAmount / 100) + " руб.");
+            System.out.println("Сумма на Вашем счету - " + (niceBonus + depositAmount + initialAmount) + " руб.");
+        }
+
+        if (depositAmount < 1000) {            // Бонуса нет
+            System.out.println("Сожалеем, суммы пополнения не хватило для начисления бонуса :( ");
+            System.out.println("Сумма на Вашем счету - " + (initialAmount + depositAmount) + " руб.");
+        }
+
+    }
+}
